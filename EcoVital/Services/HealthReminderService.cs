2032ba4281c77977f5b3f7e7556fdb046a3ad44f
xDeew <andretrolezi14@gmail.com>
2024-05-05 18:58:00 +0200

    Removiendo servicio inútil

--- EcoVital/Services/HealthReminderService.cs
+++
@@ -1,7 +0,0 @@
-namespace EcoVital.Services;
-
-public class HealthReminderService
-{
-    readonly HttpClient _client;
-    readonly string _apiBaseUrl = "https://vivaservice.azurewebsites.net/api/healthreminder";
-}
