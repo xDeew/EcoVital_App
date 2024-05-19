ebeedd5872cbe41ede81f6b89acd8cc1bd392954
xDeew <andretrolezi14@gmail.com>
2024-05-19 09:21:24 +0200

    Stop tracking Config/ApiConfig.cs

--- EcoVital/Config/ApiConfig.cs
+++
@@ -1,7 +0,0 @@
-namespace EcoVital.Config
-{
-    public static class ApiConfig
-    {
-        public static string ApiKey { get; } = "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"; // Reemplaza "sk-xxxxxx" con tu clave real
-    }
-}
