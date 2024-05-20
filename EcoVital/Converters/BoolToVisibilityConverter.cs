2e7be2f9c0af0300cb42bbbc8eda95e8ab961cd1
xDeew <andretrolezi14@gmail.com>
2024-05-20 18:41:16 +0200

    Icono chatbot y user mejora estética FINAL

---
+++ EcoVital/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,16 @@
+using System.Globalization;
+
+namespace EcoVital.Converters;
+
+public class BoolToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
